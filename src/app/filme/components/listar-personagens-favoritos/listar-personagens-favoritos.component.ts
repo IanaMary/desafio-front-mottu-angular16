f35a1f6d74ed8f7b4d5f47d12a02c6cf12b0c6a1
IanaMary <ianacosta1303@gmail.com>
2025-09-24 11:03:09 -0300

    css dos textos vazios.

--- src/app/filme/components/listar-personagens-favoritos/listar-personagens-favoritos.component.ts
+++ src/app/filme/components/listar-personagens-favoritos/listar-personagens-favoritos.component.ts
@@ -1,10 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PersonagemService } from '../../services/personagem.service';
 
 @Component({
   selector: 'app-listar-personagens-favoritos',
   templateUrl: './listar-personagens-favoritos.component.html',
   styleUrls: ['./listar-personagens-favoritos.component.scss']
 })
-export class ListarPersonagensFavoritosComponent {
-  @Input() atualizar: boolean = false;
+export class ListarPersonagensFavoritosComponent implements OnInit {
+
+  constructor(private readonly personagemService: PersonagemService) { }
+
+  personagens: any[] = [];
+  totalPersonagens = 0;
+  paginaAtual = 1;
+  nomePersonagem: string = '';
+
+  ngOnInit(): void { }
+
+  carregarPersonagens() {
+    this.personagemService.getListarFilmes(this.paginaAtual, this.nomePersonagem).subscribe({
+      next: (res: any) => {
+        this.personagens = res.results;
+        this.totalPersonagens = res.info.count;
+      },
+      error: (err: any) => {
+        this.personagens = [];
+      }
+    });
+
+  }
+
+  trackById(index: number, item: any): number {
+    return item.id; // retorna o identificador único
+  }
+
+  paginacao(e: any) {
+    this.paginaAtual = e.pageIndex + 1;
+    this.carregarPersonagens();
+  }
+
 }
